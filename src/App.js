ecbfb3825f7be17cb4505be9d7c1f98a7bdb1df8
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-10-24 23:49:17 +0300

    made a project outline

--- src/App.js
+++ src/App.js
@@ -2,7 +2,46 @@ import './App.css';
 import React from "react";
 
 const App = () => {
-
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src='https://img.freepik.com/free-vector/planet-mars-word-logo-design-with-mars-planet_1308-79520.jpg?semt=ais_hybrid'/>
+            </header>
+            <nav className="navigation">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src=''/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My post
+                    <div>
+                        New post
+                    </div>
+                </div>
+                Content
+            </div>
+        </div>
+    );
 }
 
 export default App;
