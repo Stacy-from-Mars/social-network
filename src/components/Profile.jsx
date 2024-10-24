a1297854495a16fd0167a12e19462d36d85b28be
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-10-24 23:56:19 +0300

    added the component Profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+return (
+    <div className='content'>
+        <div>
+            <img src=''/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My post
+            <div>
+                New post
+            </div>
+        </div>
+        Content
+    </div>
+)}
+
+export default Profile;
