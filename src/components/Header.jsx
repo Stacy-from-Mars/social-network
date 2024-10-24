0d93ca929d9e993a63c3d38e9d9ff97f08e89475
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-10-24 23:53:21 +0300

    added the component Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+return (
+    <header className='header'>
+        <img
+            src='https://img.freepik.com/free-vector/planet-mars-word-logo-design-with-mars-planet_1308-79520.jpg?semt=ais_hybrid'/>
+    </header>
+)}
+
+export default Header;
