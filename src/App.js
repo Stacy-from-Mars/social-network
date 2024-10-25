c98b4d8f95f413df5686639b4d8f17701c4031e5
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-10-25 18:37:21 +0300

    added components to App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
 import './App.css';
-import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img
-                    src='https://img.freepik.com/free-vector/planet-mars-word-logo-design-with-mars-planet_1308-79520.jpg?semt=ais_hybrid'/>
-            </header>
-            <nav className="navigation">
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className='content'>
-                <div>
-                    <img src=''/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My post
-                    <div>
-                        New post
-                    </div>
-                </div>
-                Content
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
