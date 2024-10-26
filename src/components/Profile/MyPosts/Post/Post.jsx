c31700bbd454daef2d4cf2f87c08c57023de0474
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-10-27 00:05:41 +0300

    added props to component Post

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img
                 src="https://play-lh.googleusercontent.com/Hv6wSP7YVpV-rZXgotOEKL5JOnwuWn_WA2dC2V3oDe0F7pw86ScZ8cL3tHZwTIrrmjg=w240-h480-rw"/>
-            My post
+            {props.message}
             <div>
+                <div>
+                    {props.likes}
+                </div>
                 <button>
                     like
                 </button>
