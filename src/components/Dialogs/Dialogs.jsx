815551e0afbe74b17c2a276a5668458911e70cc3
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-01 23:32:21 +0300

    replaced anchor tags with NavLink tags in Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import s from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog}>
-                    Lola
+                    <NavLink to="/dialogs/1">Lola</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Demi
+                    <NavLink to="/dialogs/2">Demi</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Eliza
+                    <NavLink to="/dialogs/3">Eliza</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Bella
+                    <NavLink to="/dialogs/4">Bella</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Robin
+                    <NavLink to="/dialogs/5">Robin</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
