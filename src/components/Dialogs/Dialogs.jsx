55e5fc6298e5a10d8dbdb8339bee0d44cd6d8468
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-01 23:12:09 +0300

    the component Dialogs layout was made

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className="s.dialogs">
+            <div className="dialogs-items">
+                <div className="dialog">
+                    Lola
+                </div>
+                <div className="dialog">
+                    Demi
+                </div>
+                <div className="dialog">
+                    Eliza
+                </div>
+                <div className="dialog">
+                    Bella
+                </div>
+                <div className="dialog">
+                    Robin
+                </div>
+            </div>
+            <div className="messages">
+                <div className="message">Hello!</div>
+                <div className="message">How are you?</div>
+                <div className="message">You`re done great!</div>
+            </div>
         </div>
     )
 }
