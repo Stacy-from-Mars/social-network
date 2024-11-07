3ce5f72d68ab068446cbec918d6873d4432a3c22
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-07 17:41:06 +0300

    improved style of MyPosts and Post component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,17 +3,24 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
-return (
-    <div className={s.posts}>
-        My posts
-        <div>
-            <textarea></textarea>
-            <button>Add Post</button>
-            <button>Remove</button>
+    return (
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <div>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add Post</button>
+                    <button>Remove</button>
+                </div>
+            </div>
+            <div className={s.posts}>
+                <Post message={"Hi, how are you?"} likes={15}/>
+                <Post message={"It's my first message."} likes={20}/>
+            </div>
         </div>
-        <Post message={"Hi, how are you?"} likes={15}/>
-        <Post message={"It's my first message."} likes={20}/>
-    </div>
-)}
+    )
+}
 
 export default MyPosts;
