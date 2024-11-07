355ad86be7179c4c3484bab5a436a056c716d180
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-07 17:44:39 +0300

    improved style of ProfileInfo component

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -4,7 +4,7 @@ import s from './ProfileInfo.module.css';
 const ProfileInfo = () => {
     return (
         <div className={s.content}>
-            <div className={s.profileImg}>
+            <div>
                 <img src="https://c.wallhere.com/photos/5b/22/galaxy_planet-1943031.jpg!d"/>
             </div>
             <div className={s.descriptionBlock}>
