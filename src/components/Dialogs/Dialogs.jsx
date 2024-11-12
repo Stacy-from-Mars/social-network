343d5838af642a068341f37b3bde4fc913befd8e
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-13 00:45:36 +0300

    DialogItems component have been moved from Dialogs.jsx to the DialogItems.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,6 @@
 import React from "react";
 import s from "./Dialogs.module.css";
-import {NavLink} from "react-router-dom";
-
-const DialogItems = (props) => {
-    let path = "/dialogs/" + props.id;
-
-    return (
-        <div className={s.dialog}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
+import DialogItems from "./DialogItems/DialogItems";
 
 const Message = (props) => {
     return (
@@ -34,17 +24,17 @@ const Dialogs = () => {
         {message: "You`re done great!"}
     ];
 
-    let dialogsItems = [dialogs.map(d => <DialogItems id={d.id} name={d.name}/>)];
+    let dialogsElements = dialogs.map(d => <DialogItems id={d.id} name={d.name}/>);
 
-    let messageItems = [messages.map(m => <Message message={m.message}/>)];
+    let messagesElements = messages.map(m => <Message message={m.message}/>);
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                {dialogsItems}
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                {messageItems}
+                {messagesElements}
             </div>
         </div>
     )
