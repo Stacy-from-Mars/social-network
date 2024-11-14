34cbd2ce2d51eea806fb89d5e0328ac67b36412e
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-15 00:48:31 +0300

    arrays of dialogs, messages and posts have been moved from index.jsx to the state.js

--- src/index.js
+++ src/index.js
@@ -3,30 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-let dialogs = [
-    {id: 1, name: "Lola"},
-    {id: 2, name: "Demi"},
-    {id: 3, name: "Eliza"},
-    {id: 4, name: "Bella"},
-    {id: 5, name: "Robin"}
-];
-
-let messages = [
-    {message: "Hello!"},
-    {message: "How are you?"},
-    {message: "You`re done great!"},
-];
-
-let posts = [
-    {message: "Hi, how are you?", likes: 15},
-    {message: "It's my first message.", likes: 20}
-];
+import state from "./redux/state";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App posts={posts} dialogs={dialogs} messages={messages}/>
+        <App state={state}/>
     </React.StrictMode>
 );
 
---
+++ src/redux/state.js
@@ -0,0 +1,24 @@
+let state = {
+    profilePage: {
+        posts: [
+            {message: "Hi, how are you?", likes: 15},
+            {message: "It's my first message.", likes: 20},
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: "Lola"},
+            {id: 2, name: "Demi"},
+            {id: 3, name: "Eliza"},
+            {id: 4, name: "Bella"},
+            {id: 5, name: "Robin"},
+        ],
+        messages: [
+            {message: "Hello!"},
+            {message: "How are you?"},
+            {message: "You`re done great!"},
+        ],
+    }
+}
+
+export default state;
