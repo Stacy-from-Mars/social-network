60c8bc9acc97eef156ae87bc3100659b4385ef12
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-16 14:33:23 +0300

    added sidebar data in state.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -18,6 +18,15 @@ let state = {
             {message: "How are you?"},
             {message: "You`re done great!"},
         ],
+    },
+    sidebar: {
+        friends: [
+            {id: 1, name: "Lola", image: "https://masterpiecer-images.s3.yandex.net/0b6429fb5de911ee814f963c1ee369ba:upscaled"},
+            {id: 2, name: "Demi", image: "https://masterpiecer-images.s3.yandex.net/f160d7573d1011eea399b29646062438:upscaled"},
+            {id: 3, name: "Eliza", image: "https://img.freepik.com/free-photo/3d-fox-cartoon-illustration_23-2151395236.jpg?semt=ais_hybrid"},
+            {id: 4, name: "Bella", image: "https://masterpiecer-images.s3.yandex.net/92ad4927464c11eeb50b963c1ee369ba:upscaled"},
+            {id: 5, name: "Robin", image: "https://masterpiecer-images.s3.yandex.net/4ada9dfe857111ee9b847a2f0d1382ba:upscaled"},
+        ]
     }
 }
 
