33d5a70d5380948540603bed31056edb59d90b33
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-16 14:36:33 +0300

    added randomizer to display three friends from the list in Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import s from './Navbar.module.css';
 import {NavLink} from "react-router-dom";
+import Friends from "./Friends/Friends";
 
-const Navbar = () => {
+const fillRandomFriends = (props) => {
+    let randomArr = [];
+    let friendsList = props.state.friends;
+    let generateRandomUserId = function () {
+        return Math.floor(Math.random() * friendsList.length) + 1;
+    };
+
+    let randomFunc = () => {
+        for (let i = 0; i <= 2; i++) {
+            let userId;
+            while (!userId || randomArr.find(f => f.id === userId)) {
+                userId = generateRandomUserId();
+            }
+            randomArr.push(friendsList.find(f => f.id === userId));
+        }
+    }
+    randomFunc();
+    return randomArr;
+}
+
+const Navbar = (props) => {
+    let randomFriends = fillRandomFriends(props);
+    let friendsArr = randomFriends.map(f => <Friends image={f.image} name={f.name}/>)
+
 return (
     <nav className={s.navigation}>
         <div className={s.item}>
@@ -19,6 +43,12 @@ return (
         </div>
         <div className={s.item}>
             <NavLink to={"/settings"}>Settings</NavLink>
+        </div>
+        <div className={s.item}>
+            <h3>Friends</h3>
+        </div>
+        <div className={s.friends}>
+            {friendsArr}
         </div>
     </nav>
 )}
