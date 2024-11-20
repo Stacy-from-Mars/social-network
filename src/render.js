f20ea81d15772005c22b4eca41998523b52c7044
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-20 17:50:43 +0300

    added props of updateNewPostText function in render.js

--- src/render.js
+++ src/render.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {addPost, sendMessage} from "./redux/state";
+import {addPost, sendMessage, updateNewPostText} from "./redux/state";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 export let renderEntireTree = (state) => {
-    const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} sendMessage={sendMessage}/>
+            <App state={state} addPost={addPost} sendMessage={sendMessage} updateNewPostText={updateNewPostText}/>
         </React.StrictMode>
     );
 }
