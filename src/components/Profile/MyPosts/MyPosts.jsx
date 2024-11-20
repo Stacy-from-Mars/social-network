5b98cc529d3a0906aa29cf245969c1a669c8e54f
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-20 17:59:31 +0300

    created onChange function for textarea in MyPosts component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,17 +5,22 @@ import Post from "./Post/Post";
 const MyPosts = (props) => {
 
     let postsElements = [props.posts.map(p => <Post message={p.message} likes={p.likes}/>)]
-    let addPost = () => {
-        let text = newPostElement.current.value;
-        props.addPost(text);
-    }
     let newPostElement = React.createRef();
+
+    let addPost = () => {
+        props.addPost();
+    }
+
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        props.updateNewPostText(text);
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div className={s.addPost}>
-                <textarea ref={newPostElement}/>
+                <textarea ref={newPostElement} value={props.newPostText} onChange={onPostChange}/>
                 <div>
                     <button onClick={addPost}>Add Post</button>
                     <button>Remove</button>
