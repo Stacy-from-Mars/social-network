0c7342bec614fbfd3e46274f3a8bcab8c8415fbd
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-20 20:31:41 +0300

    function renderEntireTree have been moved from render.js to index.js and renamed in rerenderEntireTree.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import './index.css';
-import {renderEntireTree} from "./render";
-import state from "./redux/state";
+import reportWebVitals from './reportWebVitals';
+import state, {addPost, sendMessage, updateNewMessageText, updateNewPostText} from "./redux/state";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-renderEntireTree(state);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+export let rerenderEntireTree = (state) => {
+    root.render(
+        <React.StrictMode>
+            <App state={state} addPost={addPost} sendMessage={sendMessage} updateNewPostText={updateNewPostText} updateNewMessageText={updateNewMessageText}/>
+        </React.StrictMode>
+    );
+}
+
+rerenderEntireTree(state);
+
+reportWebVitals();
