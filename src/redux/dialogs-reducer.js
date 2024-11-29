cc94613cf9a70c98891c843f2094f130445f2c6c
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-29 23:35:43 +0300

    all actions of dialogsPage have been moved from state.js to dialogs-reducer.js

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,5 +1,24 @@
-const dialogsReducer = (state, action) => {
+const SEND_MESSAGE = "SEND-MESSAGE";
+const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
 
+const dialogsReducer = (state, action) => {
+    switch (action.type) {
+        case SEND_MESSAGE:
+            let newMessage = {
+                message: state.newMessageText
+            }
+            state.messages.push(newMessage);
+            state.newMessageText = "";
+            return state;
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageText = action.newText;
+            return state;
+        default:
+            return state;
+    }
 }
+
+export const updateNewMessageTextCreator = (text) => ({type: UPDATE_NEW_MESSAGE_TEXT, newText: text});
+export const sendMessageCreator = () => ({type: SEND_MESSAGE});
 
 export default dialogsReducer;
