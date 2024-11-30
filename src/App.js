bb93fe0ad843bc56aeacfdf74b6619ea0cd08f03
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-11-30 23:38:18 +0300

    BrowserRouter tag have been moved from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 const App = (props) => {
     return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar state={props.state.sidebar}/>
-                <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path='/profile' element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch}/>}/>
-                        <Route path='dialogs/*' element={<Dialogs dialogsPage={props.state.dialogsPage} dispatch={props.dispatch}/>}/>
-                        {/*<Route path='news' element={<News/>}/>*/}
-                        {/*<Route path='music' element={<Music/>}/>*/}
-                        {/*<Route path='settings' element={<Settings/>}/>*/}
-                    </Routes>
-                </div>
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar state={props.state.sidebar}/>
+            <div className='app-wrapper-content'>
+                <Routes>
+                    <Route path='/profile'
+                           element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch}/>}/>
+                    <Route path='dialogs/*'
+                           element={<Dialogs dialogsPage={props.state.dialogsPage} dispatch={props.dispatch}/>}/>
+                    {/*<Route path='news' element={<News/>}/>*/}
+                    {/*<Route path='music' element={<Music/>}/>*/}
+                    {/*<Route path='settings' element={<Settings/>}/>*/}
+                </Routes>
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import reportWebVitals from './reportWebVitals';
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./redux/state";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 export let rerenderEntireTree = (state) => {
     root.render(
         <React.StrictMode>
-            <App state={state} dispatch={store.dispatch.bind(store)}/>
+            <BrowserRouter>
+                <App state={state} dispatch={store.dispatch.bind(store)}/>
+            </BrowserRouter>
         </React.StrictMode>
     );
 }
