1f79d7fffbea77fc849a6c94c0fe5e3616d2632e
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-12-03 00:08:36 +0300

    added sidebarReducer to state.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,7 @@
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
+
 
 let store = {
     _state: {
@@ -32,7 +34,8 @@ let store = {
                 {id: 3, name: "Eliza", image: "https://img.freepik.com/free-photo/3d-fox-cartoon-illustration_23-2151395236.jpg?semt=ais_hybrid"},
                 {id: 4, name: "Bella", image: "https://masterpiecer-images.s3.yandex.net/92ad4927464c11eeb50b963c1ee369ba:upscaled"},
                 {id: 5, name: "Robin", image: "https://masterpiecer-images.s3.yandex.net/4ada9dfe857111ee9b847a2f0d1382ba:upscaled"},
-            ]
+            ],
+            newFriendsArray: []
         }
     },
     _callSubscriber() {
@@ -47,8 +50,11 @@ let store = {
     dispatch(action) {
         this._state.profilePage = profileReducer(this._state.profilePage, action);
         this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+        this._state.sidebar = sidebarReducer(this._state.sidebar, action);
+
         this._callSubscriber(this._state);
     }
+
 }
 
 window.store = store;
