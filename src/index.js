2cdd94bf12a3d9f585336eb41adb426c9e293b46
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-12-03 00:11:07 +0300

    added redux-store.js instead state.js to index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import store from "./redux/state";
+import store from "./redux/redux-store";
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,6 +20,9 @@ export let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(()=> {
+    let state = store.getState();
+    rerenderEntireTree(state)
+});
 
 reportWebVitals();
