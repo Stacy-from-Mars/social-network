259a5b97cd9a0e94583fb49af04558804c5652c9
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-12-09 02:32:17 +0300

    functions interacting with the redux-store have been moved from MyPosts.jsx to MyPostsContainer.jsx and passed through props back to MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
-import {addPostCreator, updateNewPostTextCreator} from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
 
     let postsElements = [props.posts.map(p => <Post message={p.message} likes={p.likes}/>)]
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        props.dispatch(addPostCreator());
+    let onAddPost = () => {
+        props.addPost();
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.dispatch(updateNewPostTextCreator(text));
+        props.updateNewPostText(text);
     }
 
     return (
@@ -23,7 +22,7 @@ const MyPosts = (props) => {
             <div className={s.addPost}>
                 <textarea ref={newPostElement} value={props.newPostText} onChange={onPostChange}/>
                 <div>
-                    <button onClick={addPost}>Add Post</button>
+                    <button onClick={onAddPost}>Add Post</button>
                     <button>Remove</button>
                 </div>
             </div>
