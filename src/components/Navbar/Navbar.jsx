36ff6c0dc4cb6367c0107160fca4ab1f447057d9
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-12-19 20:33:21 +0300

    functions interacting with the redux-store have been moved from Navbar.jsx to NavbarContainer.jsx and passed through props back to Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import s from './Navbar.module.css';
 import {NavLink} from "react-router-dom";
-import {refreshFriendsCreator} from "../../redux/sidebar-reducer";
-import StoreContext from "../../StoreContext";
 
-const Navbar = () => {
-    return <StoreContext.Consumer>
-        {store => {
-            const refreshFriends = () => {
-                store.dispatch(refreshFriendsCreator());
-            }
-            let state = store.getState().sidebar;
+const Navbar = (props) => {
             return (
                 <nav className={s.navigation}>
                     <div className={s.item}>
@@ -32,15 +24,11 @@ const Navbar = () => {
                         <h3>Friends online</h3>
                     </div>
                     <div className={s.friends}>
-                        {state.newFriendsArray}
+                        {props.sidebar.newFriendsArray}
                     </div>
-                    <button onClick={refreshFriends}>Refresh</button>
+                    <button onClick={props.refreshFriends}>Refresh</button>
                 </nav>
             )
-        }
-        }
-    </StoreContext.Consumer>
 }
-
 
 export default Navbar;
--- src/components/Navbar/NavbarContainer.jsx
+++ src/components/Navbar/NavbarContainer.jsx
@@ -1,5 +1,19 @@
+import {refreshFriendsCreator} from "../../redux/sidebar-reducer";
+import Navbar from "./Navbar";
 import {connect} from "react-redux";
 
-const NavbarContainer = connect()()
+const mapStateToProps = (state) => {
+    return {
+        sidebar: state.sidebar
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        refreshFriends: () => dispatch(refreshFriendsCreator())
+    }
+}
+
+const NavbarContainer = connect(mapStateToProps, mapDispatchToProps)(Navbar)
 
 export default NavbarContainer;
