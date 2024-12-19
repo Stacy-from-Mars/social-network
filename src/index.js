f2a54c47c0ccb11e715a6e3d9b638e2af05f7903
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-12-19 20:02:20 +0300

    replaced Provider from StoreContext with Provider from React-Redux in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./redux/redux-store";
 import {BrowserRouter} from "react-router-dom";
-import {Provider} from "./StoreContext";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
