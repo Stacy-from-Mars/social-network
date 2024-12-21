adf7bc7213699fda4d4656d7d76d163aa92064fc
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-12-21 22:08:36 +0300

    removed rerenderEntireTree function in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./redux/redux-store";
@@ -9,23 +8,12 @@ import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-export let rerenderEntireTree = () => {
-    root.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}>
-                    <App/>
-                </Provider>
-            </BrowserRouter>
-        </React.StrictMode>
-    );
-}
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(()=> {
-    let state = store.getState();
-    rerenderEntireTree(state)
-});
-
-reportWebVitals();
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
