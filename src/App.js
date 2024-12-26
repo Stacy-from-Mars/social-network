a273bd724bfdc3878aa5a92a7c9eb415717466e3
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-12-26 23:31:27 +0300

    added route to Users component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Profile from "./components/Profile/Profile";
 import {Route, Routes} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
 const App = () => {
     return (
@@ -16,6 +17,8 @@ const App = () => {
                            element={<Profile/>}/>
                     <Route path='dialogs/*'
                            element={<DialogsContainer/>}/>
+                    <Route path='/users'
+                           element={<UsersContainer/>}/>
                     {/*<Route path='news' element={<News/>}/>*/}
                     {/*<Route path='music' element={<Music/>}/>*/}
                     {/*<Route path='settings' element={<Settings/>}/>*/}
