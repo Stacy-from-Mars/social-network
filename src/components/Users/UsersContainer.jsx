0f2a74207fe75a5b8f046673bd9eb1e86ffe7cf5
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-12-28 15:10:51 +0300

    added state and dispatch in UsersContainer.jsx

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,16 +1,18 @@
-
+import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 import Users from "./Users";
 import {connect} from "react-redux";
 
 const mapStateToProps = (state) => {
     return {
-
+        usersPage: state.usersPage.users
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-
+        follow: (userId) => dispatch(followAC(userId)),
+        unfollow: (userId) => dispatch(unfollowAC(userId)),
+        setUsers: (users) => dispatch(setUsersAC(users))
     }
 }
 
